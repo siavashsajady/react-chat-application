062412ccbcb9055e14c830c0783c41bd5001a7b8
siavashsajady <hubgitpro@gmail.com>
2022-02-07 15:22:12 +0100

    implement renderReadReceipts function

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -10,6 +10,22 @@ const ChatFeed = (props) => {
   const chat = chats && chats[activeChat];
   //   console.log(chat, userName, messages);
 
+  const renderReadReceipts = (message, isMyMessage) =>
+    chat.people.map(
+      (person, index) =>
+        person.last_read === message.id && (
+          <div
+            key={`read_${index}`}
+            className='read-receipt'
+            style={{
+              float: isMyMessage ? 'right' : 'left',
+              backgroundImage:
+                person.person.avatar && `url(${person.person.avatar})`,
+            }}
+          />
+        )
+    );
+
   //Component for Generate messages
   const renderMessages = () => {
     //fetch all of messages
@@ -38,7 +54,7 @@ const ChatFeed = (props) => {
               marginLeft: isMyMessage ? '0px' : '68px',
             }}
           >
-            read-receipts
+            {renderReadReceipts(message, isMyMessage)}
           </div>
         </div>
       );
