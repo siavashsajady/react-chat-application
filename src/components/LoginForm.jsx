d72fdb2869deb6a9c14db26520dcc8a78acc5d70
siavashsajady <hubgitpro@gmail.com>
2022-02-08 09:46:37 +0100

    implement LoginForm component

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const authObject = {
+      'Project-ID': '899e1dbf-dd0f-47a0-b4d6-186654dc7529',
+      'User-Name': username,
+      'User-Secret': password,
+    };
+
+    try {
+      await axios.get('https://api.chatengine.io/chats', {
+        headers: authObject,
+      });
+
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+
+      window.location.reload();
+      setError('');
+    } catch (err) {
+      setError('Oops, incorrect credentials.');
+    }
+  };
+
+  return (
+    <div className='wrapper'>
+      <div className='form'>
+        <h1 className='title'>Chat Application</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className='input'
+            placeholder='Username'
+            required
+          />
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className='input'
+            placeholder='Password'
+            required
+          />
+          <div align='center'>
+            <button type='submit' className='button'>
+              <span>Start chatting</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
